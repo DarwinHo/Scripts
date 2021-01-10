bfcda9dea325a6b28ac56a0488f8c26c6d27ff07
DarwinHo <darsingho@hotmail.com>
2021-01-10 13:44:37 +0800

    Update qcc.js

--- qcc.js
+++ qcc.js
@@ -6,8 +6,11 @@ let obj = JSON.parse($response.body);
 obj.result.isSVIP = true;
 obj.result.user_type = 2;
 obj.result.is_active = 1;
-obj.result.vipStartDate = 1610255024000;
-obj.result.vipEndDate = 2610255024000;
+obj.result.vipStartDate = 1610255000;
+obj.result.vipEndDate = 2610255000;
 obj.result.isVIP = true;
+obj.result.is_showEntrance = true;
+obj._extendInfo.isVIP = true;
+obj._extendInfo.isSVIP = true;
 
 $done({body: JSON.stringify(obj)});
