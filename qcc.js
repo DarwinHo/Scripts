e7d9bb81bdddd7e6215c5f56b36608c02dbbfcde
DarwinHo <darsingho@hotmail.com>
2021-01-10 13:25:11 +0800

    Create qcc.js

---
+++ qcc.js
@@ -0,0 +1,12 @@
+//
+
+
+let obj = JSON.parse($response.body);
+
+obj.result.is_vip = true;
+obj.result.wt.vip.enabled = true;
+obj.result.wt.vip.svip_expired_at = 2610806340.7131289005;
+obj.result.svip_expired_at = 2610806340.7131289005;
+obj.result.vip_type = "s";
+
+$done({body: JSON.stringify(obj)});
