10ca8682ef4096a9d9d0b9dc550d5ff6f5fccad1
DarwinHo <darsingho@hotmail.com>
2021-03-20 16:39:54 +0800

    Update ksbao.js

--- ksbao.js
+++ ksbao.js
@@ -1,24 +1,39 @@
 /*
 by Primovist
+
+{
+  "msg" : "ok",
+  "data" : {
+    "resultJson" : {
+      "isoldplans" : 0,
+      "isnewplans" : 0,
+      "allfinished" : 0,
+      "nofinished" : 0,
+      "haveceping" : 0,
+      "tasksisfinished" : 0,
+      "isnewhaveplans" : 1,
+      "alldone" : 0
+      
 Quantumult X:
 [rewrite_local]
-^https?:\/\/slb\-user\.ksbao\.com\/(api\/newauth\/|app\/user\/(pro\/stat\?|init\?|pro\/isUserProAndNotExpired\?|pay\/checkIntroOfferPeriod\?)) url script-response-body https://raw.githubusercontent.com/DarwinHo/Script/master/Surge/JS/yyzs.js
+https://slb-exam.ksbao.com/api/newplan/doing
+^https?:\/\/slb\-exam\.ksbao\.com\/(api\/newplan\/doing|app\/user\/(pro\/stat\?|init\?|pro\/isUserProAndNotExpired\?|pay\/checkIntroOfferPeriod\?)) url script-response-body https://raw.githubusercontent.com/DarwinHo/Script/master/Surge/JS/yyzs.js
 [MITM]
 hostname = newdrugs.dxy.cn
 */
 
-const path1 = "/api/newauth/";
+const path1 = "/api/newplan/doing";
 const path2 = "/app/user/pro/stat?";
 const path3 = "/app/user/init?";
 const path4 = "/app/user/pro/isUserProAndNotExpired?";
 const path5 = "/app/user/pay/checkIntroOfferPeriod?";
 
 const url = $request.url;
-let body = $response.header;
+let body = $response.body;
 
 if (url.indexOf(path1) != -1){
-#body = JSON.parse(body);
-body.Access-Control-Allow-HttpOnly = true;
+body = JSON.parse(body);
+body.data.isnewplans = 1;
 #body.items.expert = true;
 #body.items.expertStatus = 1;
 #body.items.professional = true;
