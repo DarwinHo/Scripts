23404aef07eab5cc4c5d28bca65ba3147aaeca08
DarwinHo <darsingho@hotmail.com>
2021-03-20 16:28:12 +0800

    Update ksbao.js

--- ksbao.js
+++ ksbao.js
@@ -25,46 +25,4 @@ body.Access-Control-Allow-HttpOnly = true;
 #body = JSON.stringify(body);
 }
 
-if (url.indexOf(path2) != -1){
-body = JSON.parse(body);
-body.data.userProDiscountType = 0;
-body.data.svipUserProInfo.activeType = 1;
-body.data.svipUserProInfo.placeholderVip = true;
-body.data.userProInfoVO.activeType = 1;
-body.data.userProInfoVO.placeholderVip = true;
-body.data.isActive = true;
-body.data.remainExpiredDay = 2048;
-body = JSON.stringify(body);
-}
-
-if (url.indexOf(path3) != -1){
-body = JSON.parse(body);
-body.data.isProActive = true;
-body.data.expireDate = 2048;
-body.data.userProInfoVO = true;
-body.data.identify = true;
-body.data.userProDiscountType = 0;
-body.data.memberDiscount = true;
-body.data.iapPurchaseVO.purchase = true;
-body.data.iapPurchaseVO.status = true;
-body.data.iapPurchaseVO.codeType = 1;
-body.data.iapPurchaseVO.expireDate = 2048;
-body.data.iapPurchaseVO.message = null;
-body.data.iapPurchaseVO.error = null;
-body = JSON.stringify(body);
-}
-
-if (url.indexOf(path4) != -1){
-body = JSON.parse(body);
-body.data = true;
-body = JSON.stringify(body);
-}
-
-if (url.indexOf(path5) != -1){
-body = JSON.parse(body);
-body.data.hasSubscribeProduct = true;
-body.data.hasSvipSubscribeProduct = true;
-body = JSON.stringify(body);
-}
-
 $done({body})
